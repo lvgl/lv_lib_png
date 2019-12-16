5018378ee704be467866609a259dc9b1161482a5
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2019-12-16 07:33:05 +0100

    Update lv_lodepng.c

--- lv_lodepng.c
+++ lv_lodepng.c
@@ -142,6 +142,7 @@ static lv_res_t decoder_open(lv_img_decoder_t * decoder, lv_img_decoder_dsc_t * 
 
             /*Decode the loaded image in ARGB8888 */
             error = lodepng_decode32(&img_data, &png_width, &png_height, png_data, png_data_size);
+            free(png_data); /*Free the loaded file*/
             if(error) {
                 printf("error %u: %s\n", error, lodepng_error_text(error));
                 return LV_RES_INV;
